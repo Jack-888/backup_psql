8cc885be164545e0379e61ac0f77c85d32b4044d
Sh.V <vasya.shevchenko.2011@gmail.com>
2018-05-07 17:03:27 +0300

    add error handling

--- psql_dump_script.sh
+++ psql_dump_script.sh
@@ -5,15 +5,18 @@
 #
 # First add task cron:
 # Backup db every day
-# 0 23 * * * /bin/bash -l -c 'cd /home/backup/ && ./psql_dump_script.sh >> psql_du
+# 0 23 * * * /bin/bash -l -c 'cd /home/backup/ && ./psql_dump_script.sh &>> psql_dump_script_log.log
 # ---------------------------------------------------------------------
 
-
+#seting===========================
 DATE=`date '+%Y.%m.%d %H:%M:%S'`;
 DATE_DUMP=`date '+%Y%m%d%H%M%S'`;
-PSQL_DB_NAME='pwrset_dev'
+
+PSQL_DB_NAME='pwrset_dev';
+
 DUMP_NAME="$DATE_DUMP"_"$PSQL_DB_NAME"_dump.sql;
 PATH_DUMP="$PWD/dump_db/";
+#end_seting===========================
 
 echo '\n';
 echo '=========';
@@ -21,7 +24,7 @@ echo "$DATE";
 echo 'Created dump';
 echo 'Path dump:';
 echo $PWD/"$DUMP_NAME";
-pg_dump "$PSQL_DB_NAME" --no-owner > "$PATH_DUMP""$DUMP_NAME";
+pg_dump "$PSQL_DB_NAME" --no-owner > "$PATH_DUMP""$DUMP_NAME" 
 #pg_dump name_db --no-owner > /home/backup/dump_db/name_file_dump.sql;
 echo "Dump name:";
 echo "$DUMP_NAME";
